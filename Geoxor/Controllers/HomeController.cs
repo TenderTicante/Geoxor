5c5173241a5c0c5412273d3c70b52a8b5fcac973
Tender Ticante <tenderticante@gmail.com>
2018-07-08 23:15:35 -0700

    Se agrega una base de datos de SQL Compact

--- Geoxor/Controllers/HomeController.cs
+++ Geoxor/Controllers/HomeController.cs
@@ -4,27 +4,17 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace Geoxor.Controllers
+namespace MvcDemo.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
-        {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
+        { return View(); }
 
         public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+        { return View(); }
 
-            return View();
-        }
+        public ActionResult About()
+        { return View(); }
     }
 }
